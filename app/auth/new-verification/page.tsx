e75876b152387366abc842e91efff428d6f1f924
Grig Zaqaryan <grigzaqaryan@Grigs-MacBook-Air.local>
2024-07-03 00:30:51 +0400

    Error fixing - 2

--- app/auth/new-verification/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-
-export default function NewVerifiationPage() {
-  const params = useSearchParams();
-  const token = params.get("token");
-  return <div></div>;
-}
