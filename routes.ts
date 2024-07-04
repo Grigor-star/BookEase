d63ee44bf838f0b906ddbfb0967220c359f10856
Grig Zaqaryan <grigzaqaryan@Grigs-MacBook-Air.local>
2024-07-04 18:50:15 +0400

    Fixed middleware

---
+++ routes.ts
@@ -0,0 +1,12 @@
+export const publicRoutes = ["/", "/auth/new-verification"];
+export const apiAuthPrefix = "/api/auth";
+export const authRoutes = ["/auth/login", "/auth/register"];
+export const DEFAULT_LOGIN_REDIRECT = "/store";
+export const storeRoutes = [
+  "/store/dashboard",
+  "/store/appointments",
+  "/store/invoices",
+  "/store/services",
+  "/store/team",
+  "/store/products",
+];
